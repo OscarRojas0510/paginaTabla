ffe81ff04f7932d18c5d23dddf49f4b372884a0d
OscarRojas0510 <OscarRojas0510@users.noreply.github.com>
2023-04-17 17:17:56 +0000

    Deploying to gh-pages from @ OscarRojas0510/paginaTabla@24163b14b1e2feb596110be0e9f4fe567e0cf82b 🚀

--- practicasOtras/js/scripts.js
+++ practicasOtras/js/scripts.js
@@ -1,3 +1,48 @@
-var nombre = "Oscar";
-document.write("Mi nombre es "+nombre);
-console.log("Mi nombre es "+nombre);
+'use strict';
+/*
+console.log("Hola mundo en js");
+
+let container = document.querySelectorAll('th');
+for(i = 0; i<container.length;i++)
+{
+    console.log('th numero '+i+' = '+container[i].outerText);
+}
+*/
+const num1 = 30;
+let verMas = document.querySelectorAll("a");
+console.log(verMas);
+for (let i = 0; i < verMas.length; i++) {
+  verMas[i].addEventListener("click", function (evento) 
+  {
+    let fila = this.offsetParent.offsetParent.querySelectorAll("td");
+    fila = fila[1].innerText.toLowerCase();
+    switch (fila) 
+    {
+      case "sensei":
+        console.log("dirigiendo a sensei");
+        evento.preventDefault();
+        let contenido = document.querySelector(".contenido");
+        contenido.classList.remove("animate__animated");
+        contenido.classList.remove("animate__animated");
+        contenido.classList.add("animate__animated");
+        contenido.classList.add("animate__backOutLeft");
+        setTimeout(function()
+        {
+            location.href = "/practicasOtras/restaurantes/sensei.html";
+        },600);
+        
+        break;
+      case "pepe's pizza":
+        console.log("dirigiendo a Pepe's Pizza");
+        evento.preventDefault();
+        break;
+      case "la calabaza":
+        console.log("dirigiendo a La calabaza");
+        evento.preventDefault();
+        break;
+      default:
+        console.log("Restaurante no enlistado");
+        evento.preventDefault();
+    }
+  });
+}
